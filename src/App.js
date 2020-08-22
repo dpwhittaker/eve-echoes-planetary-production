22ac435b4322d6173193d1dfebcd194bcde7ee34
Chase Thorne <nullaffinity@gmail.com>
2020-08-21 21:26:42 -0400

    bit of clean up, added distance range slider, added styles

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete, {createFilterOptions} from '@material-ui/lab/Autocomplete';
+import React, { useEffect, useMemo, useState } from 'react';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+import {
+  Slider,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+
 import './App.css';
 import data from './data/data.json';
 
-let systems = data.data;
-let systemIds = [];
-for (let i = 0; i < systems.length; i++) systemIds.push(i);
-
 function App() {
-  const [baseSystem, setBaseSystem] = React.useState(0);
+  const [baseSystem, setBaseSystem] = useState(null);
+  const [distanceRange, setDistanceRange] = useState([0, 10]);
+  const [distanceMax, setDistanceMax] = useState(100);
+  const [distanceMin, setDistanceMin] = useState(0);
+
+  const systems = useMemo(() => {
+    return data.data.map((datum) => ({
+      region: datum[0],
+      constellation: datum[1],
+      system: datum[2],
+    }));
+  }, [data]);
 
   return (
-    <div className="App">
-      <Autocomplete
-        id="baseSystem"
-        value={baseSystem}
-        onChange={(event, newValue) => {
-          console.log(newValue);
-          setBaseSystem(newValue);
-        }}
-        options={systemIds}
-        filterOptions={createFilterOptions({limit: 100})}
-        getOptionLabel={(option) => `${systems[option][0]} > ${systems[option][1]} > ${systems[option][2]}`}
-        style={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Base System" variant="outlined" />}
-      />
-      
+    <div className="app-container">
+      <div className="input-container">
+        <Autocomplete
+          filterOptions={createFilterOptions({limit: 100})}
+          getOptionLabel={(option) => `${option.region} > ${option.constellation} > ${option.system}`}
+          onChange={(event, value) => setBaseSystem(value)}
+          options={systems}
+          renderInput={(params) => <TextField {...params} label="Base System" variant="outlined" />}
+          className="input-wrapper"
+          value={baseSystem}
+        />
+        <div className="input-wrapper slider">
+          <span>Distance to System</span>
+          <Slider
+            max={distanceMax}
+            min={distanceMin}
+            onChange={(event, value) => setDistanceRange(value)}
+            step={1}
+            value={distanceRange}
+            valueLabelDisplay="on"
+          />
+        </div>
+      </div>
     </div>
   );
 }
